702c8e97d1ee801d0ab7784e4529bca1dcd851ff
Allarious <alirezaicq4@gmail.com>
2020-05-04 19:38:39 +0430

    completed subtraction and addition in recursive mathlab

--- math_lab_recursive.py
+++ math_lab_recursive.py
@@ -1,12 +1,16 @@
 class MathLabRecursive():
     def addition(self, a, b):
-        pass
+        if a == 0:
+            return b
+        return self.addition(a - 1, b + 1)
     
     def subtraction(self, a, b):
-        pass
+        if b == 0:
+            return a
+        return self.subtraction(a - 1, b - 1)
     
     def multiplication(self, a, b):
-        pass
+        
     
     def division(self, a, b):
         pass
