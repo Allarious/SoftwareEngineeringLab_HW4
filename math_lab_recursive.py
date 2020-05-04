42c1f390864fc9bbbde604a4378773864ea07cea
Allarious <alirezaicq4@gmail.com>
2020-05-04 19:46:39 +0430

    added multiplication in recursive mathlab

--- math_lab_recursive.py
+++ math_lab_recursive.py
@@ -10,7 +10,12 @@ class MathLabRecursive():
         return self.subtraction(a - 1, b - 1)
     
     def multiplication(self, a, b):
-        pass
+        def _mul(a, b, c=1):
+            if a<c:
+                return a, 0
+            a, r = _mul(a, b + b, c + c)
+            return (a - c, r + b) if a >= c else (a,r)
+        return (_mul(a, b) if a<b else _mul(b, a))[1]
     
     def division(self, a, b):
         pass
