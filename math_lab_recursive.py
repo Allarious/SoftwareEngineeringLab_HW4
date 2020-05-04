1f7d5b5284be6194a8182f43bc4dd57d52c577b1
Allarious <alirezaicq4@gmail.com>
2020-05-04 19:31:33 +0430

    making the new branch for math lab recursive

---
+++ math_lab_recursive.py
@@ -0,0 +1,12 @@
+class MathLabRecursive():
+    def addition(self, a, b):
+        pass
+    
+    def subtraction(self, a, b):
+        pass
+    
+    def multiplication(self, a, b):
+        pass
+    
+    def division(self, a, b):
+        pass
