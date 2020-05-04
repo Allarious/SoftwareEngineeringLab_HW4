9f56c3e329b128fb16063709a6b8abf7dd642637
Narges Hrh <nargesha05@gmail.com>
2020-05-04 20:39:58 +0430

    substraction method completed

--- math_lab.py
+++ math_lab.py
@@ -1,10 +1,10 @@
-class MathLab():
-    # this class is supposed to have some different mathmatical functions like sum, subtract...
+class MathLab:
+    # this class is supposed to have some different mathematical functions like sum, subtract...
     def addition(self, a, b):
-        return a + b;
+        return a + b
     
     def subtraction(self, a, b):
-        return "not done yet"
+        return a - b
     
     def division(self, a, b):
         return "not done yet"
